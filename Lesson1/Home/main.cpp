c51182454e5dd0deba884ceee3211178009050cc
mgorzelak <milena.gorzelak5@gmail.com>
2016-11-27 12:15:04 +0100

    Hello world zmienione na Hello world Home project

--- Lesson1/Home/main.cpp
+++ Lesson1/Home/main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world Home project!" << endl;
+    cout << "Hello world Home project-Hello world zmienione na Hello world Home project!" << endl;
     return 0;
 }
