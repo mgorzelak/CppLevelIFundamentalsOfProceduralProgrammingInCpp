2609c0425d9239bb30eb15acb6c3a00f80d4ac31
mgorzelak <milena.gorzelak5@gmail.com>
2017-01-13 12:45:17 +0100

    lesson 4: home tasks 1,2 and lesson task 3

---
+++ Lesson4/HomeTask1/Marek/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "imie Marek zastepione na Janek" << endl;
+
+    string imie= "Marek";
+    imie[0]='J';
+    imie[2]='n';
+    cout<<endl;
+    cout<<imie<< endl;
+    return 0;
+}
---
+++ Lesson4/LessonTask3/losowosc/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+
+using namespace std;
+
+int liczba;
+int nrProvided;
+int nrTrialas;
+
+
+int main()
+{
+    srand (time(NULL)); // Pobieramy liczbê sekund jaka uplynela od 1970 do  momentu wywolania tej linijki
+    int nrTrialas=0;
+    cout <<" wprowadz liczbe od 1 do 30"<<endl;
+    cin>>nrProvided;
+    cout<<endl;
+    cout << "Pomyslalem liczbie od 1 do 30" << endl;
+
+      do
+    {
+        nrTrialas++;
+        liczba = rand ()%30+1;
+
+        cout << "wypadla liczba: "<<liczba<<"  w probie: " <<nrTrialas<<endl;
+
+    }while (liczba != nrProvided);
+
+        cout<<endl;
+        cout << "potrzebowalismy "<<nrTrialas<<" prob(y) zeby otrzymac wprowadzona liczbe"<<nrProvided<<endl;
+
+
+    return 0;
+}
